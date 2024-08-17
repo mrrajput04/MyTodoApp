6c5d9619276eadbb7d8c6d3c020c4f7825149643
uvaish nasir <uvaishnasir@gmail.com>
2024-08-17 12:09:29 +0530

    Add todo services

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,156 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { getTodos, createTodo, updateTodo, deleteTodo } from "./todoService";
 import styles from "./page.module.css";
-
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  //editing state
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [editTitle, setEditTitle] = useState("");
+  const [editDescription, setEditDescription] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const response = await getTodos();
+    setTodos(response.data);
+  };
+
+  const handleAddTodo = async () => {
+    const newTodo = { title, description };
+    await createTodo(newTodo);
+    fetchTodos();
+    setTitle("");
+    setDescription("");
+  };
+
+  const handleUpdateTodo = async (id, completed) => {
+    await updateTodo(id, { completed });
+    fetchTodos();
+  };
+  const handleEditClick = (todo) => {
+    setIsEditing(true);
+    setEditId(todo.id);
+    setEditTitle(todo.title);
+    setEditDescription(todo.description);
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+    setEditId(null);
+    setEditTitle("");
+    setEditDescription("");
+  };
+  const handleSaveEdit = async () => {
+    try {
+      await updateTodo(editId, {
+        title: editTitle,
+        description: editDescription,
+      });
+      fetchTodos();
+      setIsEditing(false);
+      setEditId(null);
+      setEditTitle("");
+      setEditDescription("");
+    } catch (error) {
+      console.error("Error updating todo:", error);
+    }
+  };
+
+  const handleDeleteTodo = async (id) => {
+    await deleteTodo(id);
+    fetchTodos();
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className={styles.container}>
+      <div className={styles.addtodo}>
+        <h2 className="todohead">Add a Todo to the List</h2>
+        <input
+          className={styles.inputField}
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
+        <textarea
+          className={styles.description}
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <button className={styles.btn} onClick={handleAddTodo}>
+          Add Todo
+        </button>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.alltodos}>
+        <h2 className="todohead">Todo List </h2>
+        <ul className={styles.unorderedlist}>
+          {todos.map((todo) => (
+            <li className={styles.list} key={todo.id}>
+              {editId === todo.id ? (
+                <div className={styles.editForm}>
+                  <input
+                    type="text"
+                    placeholder="Edit Title"
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                    className={styles.input}
+                  />
+                  <textarea
+                    placeholder="Edit Description"
+                    value={editDescription}
+                    onChange={(e) => setEditDescription(e.target.value)}
+                    className={styles.textarea}
+                  ></textarea>
+                  <button
+                    onClick={handleSaveEdit}
+                    className={styles.saveButton}
+                  >
+                    Save
+                  </button>
+                  <button
+                    onClick={handleCancelEdit}
+                    className={styles.cancelButton}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              ) : (
+                <>
+                  <h2 className={styles.heading2}>{todo.title}</h2>
+                  <p className={styles.para}>{todo.description}</p>
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={(e) =>
+                      handleUpdateTodo(todo.id, e.target.checked)
+                    }
+                  />
+                  <button
+                    onClick={() => handleEditClick(todo)}
+                    className={styles.editButton}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className={styles.btn}
+                    onClick={() => handleDeleteTodo(todo.id)}
+                  >
+                    Delete
+                  </button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
-    </main>
+    </div>
   );
 }
